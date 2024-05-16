1677e5d99b07ab126bf74287a66cf15c35410c76
Uri Alvarez <cdmx2974@amerike.edu.mx>
2024-05-16 17:07:37 -0600

    Modificacion archivo 02_boton

--- 02_Parcial/02_Boton/02_Boton.ino
+++ 02_Parcial/02_Boton/02_Boton.ino
@@ -1,5 +1,6 @@
 const int inputPin = 7;
 int input7 = 0;
+int lastState;
 
 void setup() {
 	pinMode(inputPin, INPUT);
@@ -8,13 +9,15 @@ void setup() {
 
 void loop() {
 	input7 = digitalRead(inputPin);
-  	if(input7 == HIGH)
+  	if(lastState ==LOW && input7 == HIGH)
     {
 	Serial.println("1");
     delay(100);
 	}
-	else{
+	else if(lastState == HIGH && input7 == LOW){
  	Serial.println("0");
     delay(100);
 	}
+  lastState = input7;
 }
+//Tarea: Modificar el texto para que al presionar el boton salga "1", y cuando se deja deprecionar salgo solo un "0"
